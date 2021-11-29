39801afaa6850f6634237880bfd90cc1d1019e4f
kcj3054 <kcj3054@naver.com>
2021-11-29 19:52:30 +0900

    [#1] onClose,onError

--- src/main/java/com/example/socket/websocket/WebSocketServer.java
+++ src/main/java/com/example/socket/websocket/WebSocketServer.java
@@ -3,9 +3,7 @@ package com.example.socket.websocket;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Component;
 
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
+import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.Set;
@@ -35,5 +33,19 @@ public class WebSocketServer {
     @OnMessage
     public void onMessage(Session session, String message) {
         log.info(message);
+    }
+
+    // 통신 중 에러가 발생할 경우
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        sessions.remove(session);
+        log.warning("error: " + throwable.getMessage());
+    }
+
+    // server 또는 client 중 어느 하나가 통신을 끊는 경우
+    @OnClose
+    public void onClose(Session session) {
+        sessions.remove(session);
+        log.info("onClose: " + sessions.size());
     }
 }
